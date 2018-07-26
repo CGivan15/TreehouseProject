7772f53186b38b76c6d224d7ac3d0eb63dc480e0
Chris Givan <10444157+CGivan15@users.noreply.github.com>
2018-07-26 17:42:54 -0400

    added javascript, css, and html code

--- javascript.js
+++ javascript.js
@@ -1,5 +1,45 @@
-var x = myFunction(4, 3);    // Function is called, return value will end up in x
+// Function to Change Heading Color
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
 
-function myFunction(a, b) {
-    return a * b;            // Function returns the product of a and b
+    myButton.addEventListener('click', () => {
+        myHeading.style.color = 'blue';
+    });
+
+
+// Navigaion Bar Menu Function
+function myMenu(){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav"){
+        x.className += " responsive";
+    } else{
+        x.className = "topnav";
+    }
+}
+
+// appear function used for Header and main image
+function appear(elm, i, step, speed){
+    var x;
+    //initial opacity
+    i = i || 0;
+    //opacity increment
+    step = step || 5;
+    //time waited between two opacity increments in msec
+    speed = speed || 50; 
+
+    x = setInterval(function(){
+        //get opacity in decimals
+        var opacity = i / 100;
+        //set the next opacity step
+        i = i + step; 
+        if(opacity > 1 || opacity < 0){
+            clearInterval(x);
+            //if 1-opaque or 0-transparent, stop
+            return; 
+        }
+        //modern browsers
+        elm.style.opacity = opacity;
+        //older IE
+        elm.style.filter = 'alpha(opacity=' + opacity*100 + ')';
+    }, speed);
 }
